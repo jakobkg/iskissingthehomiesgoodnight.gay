647a536f65ca57060aade0c048ee504a3fd77be7
jakobkg <jakob@gronha.ug>
2022-10-01 23:43:32 +0200

    Readme, and some Rust conditional compilation (#4)
    * ✨ Readme, conditional compilation
    
    * add .md extension

--- server_src/src/main.rs
+++ server_src/src/main.rs
@@ -1,10 +1,12 @@
-use actix_web::{get, App, HttpServer, HttpResponse, Responder};
 use actix_files::Files;
-use regex::Regex;
-use std::fs;
-use serde::Deserialize;
-use rand;
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
 use lazy_static::lazy_static;
+use rand;
+use regex::Regex;
+use serde::Deserialize;
+use std::fs;
+
+#[cfg(prod)]
 use openssl::ssl::{SslAcceptor, SslFiletype, SslMethod};
 
 #[derive(Debug, Deserialize)]
@@ -26,19 +28,28 @@ struct ContributorStruct {
 }
 
 lazy_static! {
-    static ref TEMPLATE: String = fs::read_to_string("index.html")
-        .expect("Burde kunnet lese denne");
-    static ref CONTRIBUTORS: Vec<Contributor> = match serde_json::from_str(&fs::read_to_string("contributors.json")
-        .expect("Burde kunnet lese denne")) {
-        Ok(s) => {s},
-        Err(_) => {ContributorStruct {
-            contributors: Vec::new(),
-        }},
-    }.contributors;
+    static ref TEMPLATE: String =
+        fs::read_to_string("index.html").expect("Burde kunnet lese denne");
+    static ref CONTRIBUTORS: Vec<Contributor> = match serde_json::from_str(
+        &fs::read_to_string("contributors.json").expect("Burde kunnet lese denne")
+    ) {
+        Ok(s) => {
+            s
+        }
+        Err(_) => {
+            ContributorStruct {
+                contributors: Vec::new(),
+            }
+        }
+    }
+    .contributors;
 }
 
-static CNAME: &str = if cfg!(debug_assertions) {"https://localhost:8080"} else {"https://iskissingthehomiesgoodnight.gay"};
-
+static CNAME: &str = if cfg!(debug_assertions) {
+    "http://localhost:8080"
+} else {
+    "https://iskissingthehomiesgoodnight.gay"
+};
 
 fn make_div<S: Into<String> + std::fmt::Display>(service: S, name: S) -> String {
     format!("<div><a href=\"https://{service}.com/{name}\"><img src=\"{CNAME}/assets/{service}.png\">: {name}</a></div>")
@@ -48,7 +59,7 @@ fn make_div<S: Into<String> + std::fmt::Display>(service: S, name: S) -> String 
 async fn front() -> impl Responder {
     let script_re = Regex::new("/\\* script insert \\*/").unwrap();
     let style_re = Regex::new("/\\* style insert \\*/").unwrap();
-    
+
     let name_re = Regex::new("<!-- name insert -->").unwrap();
     let gh_re = Regex::new("<!-- github insert -->").unwrap();
     let twitter_re = Regex::new("<!-- twitter insert -->").unwrap();
@@ -62,51 +73,72 @@ async fn front() -> impl Responder {
     template = gh_re.replace(&template, make_div("github", gh)).to_string();
 
     match &contributor.personalia.displayname {
-        Some(displayname) => {template = name_re.replace(&template, format!("Style by {displayname}")).to_string()},
-        None => {},
+        Some(displayname) => {
+            template = name_re
+                .replace(&template, format!("Style by {displayname}"))
+                .to_string()
+        }
+        None => {}
     }
 
     match &contributor.personalia.twitter {
-        Some(twitter) => {template = twitter_re.replace(&template, make_div("twitter", twitter)).to_string()},
-        None => {},
+        Some(twitter) => {
+            template = twitter_re
+                .replace(&template, make_div("twitter", twitter))
+                .to_string()
+        }
+        None => {}
     }
 
     let style = match fs::read_to_string(format!("dist/styles/{gh}/{stylename}.css")) {
-        Ok(content) => {content},
-        Err(_) => {"".to_owned()},
+        Ok(content) => content,
+        Err(_) => "".to_owned(),
     };
 
     let script = match fs::read_to_string(format!("dist/scripts/{gh}/{stylename}.js")) {
-        Ok(content) => {content},
-        Err(_) => {"".to_owned()},
+        Ok(content) => content,
+        Err(_) => "".to_owned(),
     };
 
     println!("Responding with style {stylename} by {gh}");
 
-    let inserted = style_re.replace(&script_re.replace(&template, script), style).to_string();
+    let inserted = style_re
+        .replace(&script_re.replace(&template, script), style)
+        .to_string();
     HttpResponse::Ok().body(inserted)
 }
 
+#[cfg(prod)]
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let mut builder = SslAcceptor::mozilla_intermediate(SslMethod::tls()).unwrap();
+
     builder
         .set_private_key_file("key.pem", SslFiletype::PEM)
         .unwrap();
+
     builder.set_certificate_chain_file("cert.pem").unwrap();
 
+    println!("Server starting...");
+
+    HttpServer::new(|| {
+        App::new()
+            .service(front)
+            .service(Files::new("/assets", "./assets"))
+    })
+    .bind_openssl("iskissingthehomiesgoodnight.gay:443", builder)?
+    .run()
+    .await
+}
+
+#[cfg(debug_assertions)]
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
     println!("Server starting...");
     HttpServer::new(|| {
         App::new()
             .service(front)
             .service(Files::new("/assets", "./assets"))
     })
-    .bind_openssl(
-        if cfg!(debug_assertions) {
-            "localhost:8080"
-        } else {
-            "iskissingthehomiesgoodnight.gay:443"
-        }, builder)?
-    .run()
-    .await
+    .bind(("localhost", 8080))?.run().await
 }
